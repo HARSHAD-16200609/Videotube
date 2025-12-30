73fc29ee505c7190ca4826274602bb8c16521874
harshad diwate <harshaddiwate85@gmail.com>
2025-12-30 15:40:13 +0530

    feat: enhance like functionality for videos, comments, and tweets with improved response messages and add aggregation for fetching liked videos

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -13,18 +13,16 @@ const toggleVideoLike = async_handler(async (req, res) => {
     const existingLike = await Like.findOne({
         video: videoId,
         likedBy: userId,
-        
     });
-    if (existingLike){
+    if (existingLike) {
         await existingLike.deleteOne();
-        return res.status(200)
-            .json(new Api_Response(200,{}, "Unliked the Video Successfully"));
-    } 
-    else {
+        return res
+            .status(200)
+            .json(new Api_Response(200, {}, "Unliked the Video Successfully"));
+    } else {
         const like = await Like.create({
             video: new mongoose.Types.ObjectId(videoId),
             likedBy: userId,
-           
         });
         if (!like) throw new API_Error(500, "Failed to like the video");
 
@@ -45,18 +43,16 @@ const toggleCommentLike = async_handler(async (req, res) => {
     const existingLike = await Like.findOne({
         comment: commentId,
         likedBy: userId,
-        
     });
     if (existingLike) {
         await existingLike.deleteOne();
-     return res.status(200)
-            .json(new Api_Response(200,{}, "Unliked the Video Successfully"));
-    }
-    else {
+        return res
+            .status(200)
+            .json(new Api_Response(200, {}, "Unliked the Video Successfully"));
+    } else {
         const like = await Like.create({
             comment: commentId,
             likedBy: userId,
-            
         });
         if (!like) throw new API_Error(500, "Failed to like the comment");
         return res
@@ -68,7 +64,7 @@ const toggleCommentLike = async_handler(async (req, res) => {
 });
 
 const toggleTweetLike = async_handler(async (req, res) => {
-const { tweetId } = req.params;
+    const { tweetId } = req.params;
     //TODO: toggle like on tweet
 
     const userId = req.user._id;
@@ -78,32 +74,80 @@ const { tweetId } = req.params;
     const existingLike = await Like.findOne({
         tweet: tweetId,
         likedBy: userId,
-    
     });
     if (existingLike) {
         await existingLike.deleteOne();
-     return res.status(200)
-            .json(new Api_Response(200,{}, "Unliked the tweet Successfully"));
-    }
-    else {
+        return res
+            .status(200)
+            .json(new Api_Response(200, {}, "Unliked the tweet Successfully"));
+    } else {
         const like = await Like.create({
             tweet: tweetId,
             likedBy: userId,
-          
         });
         if (!like) throw new API_Error(500, "Failed to like the tweet");
         return res
             .status(200)
             .json(new Api_Response(200, like, "Liked the tweet Successfully"));
     }
-
-
-
-
 });
 
 const getLikedVideos = async_handler(async (req, res) => {
+    //TODO: get all liked videos
+    const userId = req.user._id;
 
+    const LikedVideos = await Like.aggregate([
+        {
+            $match: {
+                likedBy: userId,
+                comment: null,
+                tweet: null,
+            },
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "video",
+                foreignField: "_id",
+                as: "LikedVideo",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "Owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        avatar: 1,
+                                        username: 1,
+                                    },
+                                },
+                            ],
+                        },
+                    },
+                    { $unwind: "$Owner" },
+                    {
+                        $project: {
+                            videoFile: 1,
+                            thumbnail: 1,
+                            title: 1,
+                            duration: 1,
+                            views: 1,
+                            Owner: 1,
+                        },
+                    },
+                ],
+            },
+        },
+        { $unwind: "$LikedVideo" },{$replaceRoot:{newRoot:"$LikedVideo"}}
+    ]);
+    if (!LikedVideos)
+        throw new API_Error(500, "Failed to fetch the Liked Videos");
+    return res
+        .status(200)
+        .json(new Api_Response(200, LikedVideos, "Videos Fetched Suceesfully"));
 });
 
 export { toggleCommentLike, toggleTweetLike, toggleVideoLike, getLikedVideos };
